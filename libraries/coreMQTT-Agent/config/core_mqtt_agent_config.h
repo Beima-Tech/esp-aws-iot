4f386eb1eb08a0ade9998c9ff4957c5a348c812d
Jason <GASEN1216@163.com>
2026-06-26 09:00:30 +0800

    fix: configure MQTT agent outstanding ACK limit

--- libraries/coreMQTT-Agent/config/core_mqtt_agent_config.h
+++ libraries/coreMQTT-Agent/config/core_mqtt_agent_config.h
@@ -1,7 +1,9 @@
+#include "sdkconfig.h"
+
 /* Wait only a short interval each iteration for faster
  * processing of incoming data. */
 #define MQTT_AGENT_MAX_EVENT_QUEUE_WAIT_TIME       ( 10U )
 
 /* We will be downloading the OTA image one block at a time.
  * Limit the number of outstanding ACK we need. */
-#define MQTT_AGENT_MAX_OUTSTANDING_ACKS            ( 10U )
+#define MQTT_AGENT_MAX_OUTSTANDING_ACKS            CONFIG_MQTT_AGENT_MAX_OUTSTANDING_ACKS
